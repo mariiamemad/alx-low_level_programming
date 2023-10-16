157304daac446209fb19be2a9bb2d83a99037043
mariiamemad <mariiammemad@gmail.com>
2023-10-16 15:06:11 -0700

    k

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - gets the length of array
+*@s: array s
+*Return: length of array
+*/
+int _strlen(char *s)
+{
+  int x = 0;
+  int *a = &s;
+  while(a){
+    x++;
+    a++;
+  }
+  return x;
+}
