950108c10414adf355957f51b35782e8b0a99fe1
mariiamemad <mariiammemad@gmail.com>
2023-10-16 14:58:33 -0700

    k

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*swap_int - swap a b 
-*@a, *@b: parameters  targeted
-*Return : nothing
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
 */
 
 void swap_int(int *a, int *b)
 {
-int temp=*a;
-*a=*b;
-*b=temp;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
-
