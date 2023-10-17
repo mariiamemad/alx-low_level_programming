5720195b76579ad1dc80dfebafee91916ef55020
mariiamemad <mariiammemad@gmail.com>
2023-10-16 17:00:44 -0700

    k

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 int len = 0, index = 0;
 len = _strlen(s);
 
-for(index = len - 1; index >= 0; index--)
+for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 
 _putchar('\n');
@@ -25,7 +25,7 @@ int _strlen(char *s)
 {
 int c, i;
 i = 0;
-for(c = 0; s[c] != '\0'; c++)
+for (c = 0; s[c] != '\0'; c++)
 i++;
 return (i);
 }
