c94acb4d79da17ddf01db59c60deddf84e6379a4
mariiamemad <mariiammemad@gmail.com>
2023-11-27 16:29:43 -0800

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,39 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of file to be read
+ * @letters: number of letters to print
+ * Return: the actual number of letters it could read and print
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, w, len = 0;
+	ssize_t print = 0, rd = 0;
+	int fd = 0;
+	char *buf = malloc(letters);
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
+	if (!buf)
+		return (0);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+	rd = read(fd, buf, letters);
+	if (rd == -1)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		free(buf);
+		close(fd);
+		return (0);
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+	print = write(STDOUT_FILENO, buf, rd);
+	if (print == -1 || print != rd)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (print);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: name of the file
- * @text_content: NULL terminated string to add at the end of the file
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w_check, i;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_APPEND | O_WRONLY);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
